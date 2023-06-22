7dce9b8dfdd8393db80def2b88ef7f326776120d
mlouka <72737407+MagdyLouka@users.noreply.github.com>
2023-06-22 14:44:43 +0200

    Add files via upload

--- Produce_Higgs.cc
+++ Produce_Higgs.cc
@@ -6,107 +6,62 @@
 #include "Produce_Higgs.h"
 #include <iostream>
 #include <string>
-using namespace std;
 
-int main(){
-  string collider;
-  double cme;
-  string decayC;
-  double Xsection;
-  double BR_value;
-  cout<<"______________________________________________________________________________ \n";
-  cout<<"\n";
-  cout<<" Hello :) \n";
-  cout<<" You are going to produce the Higgs boson\n";
-  cout<<"______________________________________________________________________________\n";
-  cout<<"\n";
-  cout<<" Choose from the available collideres:- \n";
-  cout<<"                                     'pp' for proton-proton collider  \n";
-  cout<<"                                     'ee' for electron-positron collider \n";
-  cout<<"______________________________________________________________________________\n";
-  cout<<" The collider is: "; cin>>collider; // reading the collider type from the user
-  cout<<"\n";
-  
+int main(){ //main
+  std::string collider;
+  int CME;
+  std::string decayC;
+  std::unordered_set<int> cme_pp{8,13,14,33,40,60,80,100};
+  std::unordered_set<int> cme_ee{200,220,240,250,260,280,300,320,340,360};
+  std::unordered_set<std::string> decays{"bb","tt","mm","cc","gg","phph","zph","ww","zz"};
+
+  std::cout<<"______________________________________________________________________________ \n";
+  std::cout<<"\n";
+  std::cout<<" Hello :) \n";
+  std::cout<<" You are going to produce the Higgs boson\n";
+  std::cout<<"______________________________________________________________________________\n";
+  std::cout<<"\n";
+  std::cout<<" Choose from the available collideres:- \n";
+  std::cout<<"                                     'pp' for proton-proton collider  \n";
+  std::cout<<"                                     'ee' for electron-positron collider \n";
+  std::cout<<"______________________________________________________________________________\n";
+  std::cout<<" The collider is: "; std::cin>>collider;
+  std::cout<<"\n";
+  std::cout<<" Please choose the c.m collision energies available\n";
+  if (collider!="pp" && collider!="ee") {std::cout<<" Please enter a valid collider option \n"; exit(0);}
   if (collider=="pp"){
-    cout<<" Please choose the c.m collision energy\n";
-    cout<<" The c.m energies available are: \n";
-    cout<<"                                   8 / 13 / 14 / 33 / 40 / 60 / 80 / 100 [TeV]\n";
-    cout<<" The energy is: ";cin>>cme; // reading the center of mass energy from the user
-    cout<<"\n";
-
-  if (cme!=8 && cme!=13 && cme!=14 && cme!=33 && cme!=40 && cme!=60 && cme!=80 && cme!=100) {cout<<" Sorry ... this collision energy is not available ... Please choose from the list \n"; exit(0);}
+    for(auto &value: cme_pp) {std::cout << value << " / ";}
+    std::cout<<" The energy is[TeV]: "; std::cin>>CME;
+    if(cme_pp.find(CME)==cme_pp.end()) {std::cout << "Collision Energy not available \n";exit(0);}
   }
-  
   if (collider=="ee"){
-    cout<<" Please choose the c.m collision energy\n";
-    cout<<" The c.m energies available are: \n";
-    cout<<"                                   200 / 220 / 240 / 250 / 260 / 280 / 300 / 320 / 340 / 360 [GeV]\n";
-    cout<<" The energy is: ";cin>>cme; // reading the center of mass energy from the user 
-    cout<<"\n";
-    
-  if (cme!=200 && cme!=220 && cme!=240 && cme!=250 && cme!=260 && cme!=280 && cme!=300 && cme!=320 && cme!=340 && cme!=360) {cout<<" Sorry ... this collision energy is not available ... Please choose from the list \n"; exit(0);}
+    for(auto &value: cme_ee) {std::cout << value << " / ";}
+    std::cout<<" Energy is[GeV]: "; std::cin>>CME;
+    if(cme_ee.find(CME)==cme_ee.end()) {std::cout << "Collision Energy not available \n";exit(0);}
   }
-  else if (collider!="pp" && collider!="ee") {cout<<" Please enter a valid collider option \n"; exit(0);}
- 
-  draw graph; // creating an object of the class "draw"  
-  graph.Collision(collider,cme); // calling a class member (method)
-  cout<<"________________________________________________________________________________\n";
-  cout<<" Now let's decay the Higgs boson \n";
-  cout<<" Please choose from the available final states:- \n";
-  cout<<"                                     'bb'   for b anti-b quarks   (H → bb)   \n";                                                   
-  cout<<"                                     'tt'   for t anti-t quarks   (H → ττ)   \n";
-  cout<<"                                     'mm'   for muon anti-muon    (H → μμ)   \n";
-  cout<<"                                     'cc'   for c anti-c quarks   (H → cc)   \n";
-  cout<<"                                     'gg'   for di-gluons         (H → gg)   \n";
-  cout<<"                                     'phph' for di-photons        (H → γγ)   \n";
-  cout<<"                                     'zph'  for Z boson+photon    (H → Zγ)   \n";
-  cout<<"                                     'ww'   for WW bosons         (H → WW)   \n";
-  cout<<"                                     'ZZ'   for ZZ bosons         (H → ZZ)   \n";
-  cout<<"________________________________________________________________________________\n";
-  cout<<" Please enter the channel: "; cin>>decayC;
-  if (collider=="pp"){ // declaration of the cross sections values  
-    if (cme==8) Xsection = 19.31;
-    if (cme==13) Xsection = 48.61;
-    if (cme==14) Xsection = 50.35;
-    if (cme==33) Xsection = 178.3;
-    if (cme==40) Xsection = 231.9;
-    if (cme==60) Xsection = 394.4;
-    if (cme==80) Xsection = 565.1;
-    if (cme==100) Xsection = 740.3;
-  }
-  
-  if (collider=="ee"){ // declaration of the cross sections values
-    if (cme==200) Xsection = 0.004;
-    if (cme==220) Xsection = 0.092;
-    if (cme==240) Xsection = 0.200;
-    if (cme==250) Xsection = 0.219;
-    if (cme==260) Xsection = 0.214;
-    if (cme==280) Xsection = 0.203;
-    if (cme==300) Xsection = 0.190;
-    if (cme==320) Xsection = 0.176;
-    if (cme==240) Xsection = 0.165;
-    if (cme==360) Xsection = 0.155;
-  }
-  
-  string frag1, frag2; // initiating the two decay products
-  // declaration of the branching fraction values
-  if (decayC=="bb") {BR_value= 0.577; frag1="b" ; frag2="b~";}                        
-  if (decayC=="tt") {BR_value= 0.0632; frag1="t" ; frag2="t~";}                     
-  if (decayC=="mm") {BR_value= 0.00022; frag1="mu-" ; frag2="mu+";}
-  if (decayC=="cc") {BR_value= 0.0291; frag1="c" ; frag2="c~";}                                                                                                                                                  
-  if (decayC=="gg") {BR_value= 0.0857; frag1="g" ; frag2="g";}                                                                                                                                                   
-  if (decayC=="phph") {BR_value= 0.00228; frag1="ph" ; frag2="ph";}                                                                                                  
-  if (decayC=="zph") {BR_value= 0.00154; frag1="Z" ; frag2="ph";}                                                                                                                    
-  if (decayC=="ww") {BR_value= 0.215; frag1="W" ; frag2="W";}                           
-  if (decayC=="zz") {BR_value= 0.0264; frag1="Z" ; frag2="Z";}        
+  Collision(collider,CME); // plotting the collision
+  std::cout<<"________________________________________________________________________________\n";
+  std::cout<<" Now let's decay the Higgs boson \n";
+  std::cout<<" Please choose from the available final states:- \n";
+  std::cout<<"                                     'bb'   for b anti-b quarks   (H --> bb)   \n";                                                   
+  std::cout<<"                                     'tt'   for t anti-t quarks   (H --> ττ)   \n";
+  std::cout<<"                                     'mm'   for muon anti-muon    (H --> μμ)   \n";
+  std::cout<<"                                     'cc'   for c anti-c quarks   (H --> cc)   \n";
+  std::cout<<"                                     'gg'   for di-gluons         (H --> gg)   \n";
+  std::cout<<"                                     'phph' for di-photons        (H --> γγ)   \n";
+  std::cout<<"                                     'zph'  for Z boson+photon    (H --> Zγ)   \n";
+  std::cout<<"                                     'ww'   for WW bosons         (H --> WW)   \n";
+  std::cout<<"                                     'ZZ'   for ZZ bosons         (H --> ZZ)   \n";
+  std::cout<<"________________________________________________________________________________\n";
+  std::cout<<" Please enter the channel: "; std::cin>>decayC;
+  if(decays.find(decayC)==decays.end()) {std::cout << " Please choose a valid channel \n";exit(0);}
 
-  graph.Feynman_diagram(collider,frag1,frag2);   // calling a class member (method)                                                                                                                                                                               
-  PH Higgs_p; // creating an object of the class "PH"
-  Higgs_p.set_values(cme,BR_value,Xsection); // applying a method of the class which set the parameters
-  cout<<"\n";
-  cout<<" The total cross section is: "<<Higgs_p.calculateXsection()<<" [Pb]\n"; // applying a method and print the result
-  cout<<" The cross section . "<<decayC<<" braching fraction is: "<<Higgs_p.calculateXsection_BR()<<" [Pb]\n"; // applying another method and print the result
-  cout<<" Done \n";
-  
+  Feynman_diagram(collider, decayC);   // Plotting the Feynman diagram
+
+  Higgs test;   // initiating the Higgs class object
+  std::cout<<" Results:- \n";
+  std::cout<<" The total cross-sction is: "<<test.calculateXsection(collider,CME)<<" Pb\n"; // Calling the first member function
+  std::cout<<" The cross-section x "<<decayC<<" branching ratio is: "<<test.calculateXsection_BR(collider,CME,decayC)<<" Pb\n"; // Calling the second member function
+  std::cout<<" Done. \n";
   return 0;
-} // end main
+}//end main
--- Produce_Higgs.h
+++ Produce_Higgs.h
@@ -1,64 +1,69 @@
-//---------------------------------------
-// Creating two classes "PH" and "draw"
-//---------------------------------------
-#include<iostream>
-using namespace std;
+#include <iostream>
+#include <map>
+#include <string>
+#include <unordered_set>
 
 //creating the calculator class                       
-class PH {
-  double BR; // Higgs decay branching fraction
-  double CME; // center of mass enery
-  double Xsection; // total cross section
+class Higgs{
+private:
+  // Data members of the class (Maps that hold the collision data)
+  std::map<std::string, double> BR_map{ {"bb",0.577},{"tt",0.0632},{"mm", 0.00022},{"cc",0.0291},{"gg", 0.0857},{"phph",0.00228},{"zph",0.00154},{"ww",0.215},{"zz",0.0264} };
+  std::map<double, double> Xsection_pp{ {8,19.31},{13,48.61},{14,50.35},{33,178.3},{40,231.9},{60,394.4},{80,565.1},{100,740.3} };
+  std::map<double, double> Xsection_ee{ {200.0,0.004},{220.,0.092},{240.,0.200},{250.,0.219},{260.,0.214},{280.,0.203},{300.,0.190},{320.,0.176},{340.,0.165},{360.,0.155} };
   
 public:
-  void set_values(double x, double y, double z){ // a member function that set the parameters
-    CME = x;
-    BR = y;
-    Xsection = z;
+  double y;
+  std::string collider;
+  // A class method to caculate the coss secion using two arguments fitched form the user
+  double calculateXsection(std::string collider, int userx) {
+    if(collider=="pp") y = Xsection_pp[userx];
+    if(collider=="ee") y = Xsection_ee[userx];
+    return y;
   }
-  // a member function which returns the fudicial cross section based on the c.m.e
-  double calculateXsection() {
-    return Xsection;
-  }
-  // a member function which returns the cross section times the branching fraction                                                                                                         
-  double calculateXsection_BR() {
-    return Xsection * BR;
+  double l;
+  double m;
+  // A class method to caculate the coss secion times the BR using three arguments fitched form the user  
+  double calculateXsection_BR(std:: string collider, double userl, std::string userm) {
+    if(collider=="pp") l = Xsection_pp[userl];
+    if(collider=="ee") l = Xsection_ee[userl];    
+    m = BR_map[userm];
+    return l * m;
   }
 };
-//-----------------------------------------
-  //creating the graphics class
-  class draw {
-  public:
-    string beam2, string1, string2, string3;
-    int energy;
-    // a member function that prints a schematic diagram of the collision type
-    void Collision(string beam2, int energy){
-      if (beam2=="pp") {string1 = "Protons"; string2 = "p"; string3 = "p";}
-      if (beam2=="ee") {string1 = "Electrons"; string2 = "e-"; string3 = "e+";}
-      cout<<"_____________________________________________________________\n";
-      cout<<"\n";
-      cout<<" Lets Collide "<<string1<<" with a c.m.e of "<<energy<<" TeV\n";
-      cout<<"  "<<string2<<"  ---------->    <----------  "<<string3<<"  "<<"\n";
-      cout<<"_____________________________________________________________\n";
-    }
-    
-    string beam1, d1,d2;
-    // a member function that prints the leading order Feynman diagram
-    void Feynman_diagram(string beam1, string d1, string d2){
-      if (beam1=="pp") {string1 = "Protons"; string2 = "p"; string3 = "p";}
-      if (beam1=="ee") {string1 = "Electrons"; string2 = "e-"; string3 = "e+";}
-      cout<<"______________________________________________________________\n";
-      cout<<" "<<string2<<"                                   "<<d1<<"   \n";
-      cout<<"      *                           *                         \n";
-      cout<<"        *                      *                            \n";
-      cout<<"          *                 *                               \n";
-      cout<<"           ******( H )*****                                 \n";
-      cout<<"          *                 *                               \n";
-      cout<<"        *                      *                            \n";
-      cout<<"      *                           *                         \n";
-      cout<<" "<<string3<<"                                   "<<d2<<"   \n";
-      cout<<"_____________________________________________________________\n";
-      
-    }
-   
-  };
+
+//----------------------------------------------------------------------------------
+//Two fuctions for the graphics
+std::string userbeam, decay, b1,b2,d1,d2;
+// Maps that hold the letters of the beam paticles and the fragments
+std::map<std::string,std::string> frag1{ {"bb","b"},{"tt","t"},{"mm","mu-"},{"cc","c"},{"gg","g"},{"phph","ph"},{"zph","z"},{"ww","w-"},{"zz","z"} };
+std::map<std::string,std::string> frag2{ {"bb","b~"},{"tt","t~"},{"mm","mu+"},{"cc","c~"},{"gg","g"},{"phph","ph"},{"zph","ph"},{"ww","w+"},{"zz","z"} };
+std::map<std::string,std::string> beam1{ {"pp","p"},{"ee","e-"} };
+std::map<std::string,std::string> beam2{ {"pp","p"},{"ee","e+"} };
+
+void Collision(std::string userbeam, int energy){
+  b1 = beam1[userbeam];
+  b2 = beam2[userbeam];
+  std::cout<<"_____________________________________________________________\n";
+  std::cout<<"\n";
+  std::cout<<" Lets Collide "<<userbeam<<" with a c.m.e of "<<energy<<"\n";
+  std::cout<<"  "<<b1<<"  ---------->    <----------  "<<b2<<"  "<<"\n";
+  //std::cout<<"_____________________________________________________________\n";
+}
+
+void Feynman_diagram(std::string userbeam, std::string decay ){
+  b1 = beam1[userbeam];
+  b2 = beam2[userbeam];
+  d1 = frag1[decay];
+  d2 = frag2[decay];
+  //std::cout<<"______________________________________________________________\n";
+  std::cout<<" "<<b1<<"                                     "<<d1<<"   \n";
+  std::cout<<"      *                           *                         \n";
+  std::cout<<"        *                      *                            \n";
+  std::cout<<"          *                 *                               \n";
+  std::cout<<"           ******( H )*****                                 \n";
+  std::cout<<"          *                 *                               \n";
+  std::cout<<"        *                      *                            \n";
+  std::cout<<"      *                           *                         \n";
+  std::cout<<" "<<b2<<"                                     "<<d2<<"   \n";
+  std::cout<<"_____________________________________________________________\n";
+}
